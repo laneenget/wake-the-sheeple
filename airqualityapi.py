e8e2e9f6a15956b0b0ff989cbc16555d9fc72164
Lane Enget <lane.enget@go.minneapolis.edu>
2020-03-01 11:16:40 -0600

    Add logic for airquality api

--- airqualityapi.py
+++ airqualityapi.py
@@ -7,6 +7,11 @@ def get_aq(lat, lon):
     key = os.environ.get('WEATHER_KEY')
     user_id = os.environ.get('AERIS_USER')
     lat, lon = issapi.get_iss()
-    data = requests.get(f'https://api.aerisapi.com/airquality/{lat, lon}?&format=json&fields=loc&client_id=D8cUXU9JP0KKpZzvNLcZM&client_secret=OPms7ZI8eXEmNCK0U0keQJNSmw4nxlsoMOkP')
-    aq = data['response']['periods']['category']
-    return aq
+    query = {'p': 'lat' + ',' + 'lon', 'format': 'json', 'client_id': user_id, 'client_secret': key}
+    url = ('https://api.aerisapi.com/airquality/')
+    data = requests.get(url, params=query).json()
+    if data['success']:
+        aq = data['response'][0]['periods'][0]['category']
+        return aq
+    else:
+        return None
