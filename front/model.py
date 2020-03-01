a2b20a94587a17dcc39470a32d4851fd9687639e
Molly Clabots <molly.clabots@gmail.com>
2020-03-01 11:31:35 -0600

    Adding model

--- front/model.py
+++ front/model.py
@@ -9,3 +9,16 @@ class Bookmarks():
         self.country_name = country_name
         self.state_name = state_name
         self.city_name = city_name
+    
+    def __str__(self):
+        return f'The ISS flew over {self.lat}, {self.lng} at {self.time} on {self.date}.'
+
+    def conspiracy(self):
+        if (self.earth_quake and self.air_quality):
+            print(f'SOMEHOW, the air quality is bad and there was an earthquake. Right under the satelite, so weird, must be the world government.')
+        elif(self.earth_quake):
+            print(f'SOMEHOW, there was an earthquake right under the International Space Station. So wierd, must be the illuminatity')
+        elif(self.air_quality):
+            print(f'SOMEHOW, the air quality below the Internation Space Station just HAPPENS to be bad. Weird, must be the illuminaty world government.')
+        else:
+            print(f'All is well, the world government is sparing us today.')
